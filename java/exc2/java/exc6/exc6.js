85dec72c622fc11bab31896d41dd6c3cc53b0ade
Spindaj <107429735+Spindaj@users.noreply.github.com>
2023-04-03 14:30:40 -0300

    Delete java directory

--- java/exc2/java/exc6/exc6.js
+++
@@ -1,4 +0,0 @@
-var sequence = [1, 1, 2, 3, 5, 8, 13];
-for (var i = 0; i < sequence.length; i++) {
-  document.write(sequence[i]);
-}
--- java/exc3/exc3.js
+++
@@ -1,6 +0,0 @@
-let mult = 0
-for(i = 0; i <= 500; i++){
-    mult = i * i
-
-    document.write(mult + ' ')
-}
